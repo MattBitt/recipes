70612642b82ab06a4edb0d44596a83f174e61b73
Matt <mattsbitt@gmail.com>
2014-08-30 11:45:48 -0500

    removed login forms

--- app/forms.py
+++ app/forms.py
@@ -8,54 +8,7 @@ from app import db
 
 
 
-class SignupForm(Form):
-  name = TextField("First name",  validators = [DataRequired("Please enter your first name.")])
-  email = TextField("Email",  validators = [DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators = [DataRequired("Please enter a password.")])
-  submit = SubmitField("Create account")
- 
-  def __init__(self, *args, **kwargs):
-    Form.__init__(self, *args, **kwargs)
- 
-  def validate(self):
-    if not Form.validate(self):
-      return False
-     
-    user = User.query.filter_by(email = self.email.data.lower()).first()
-    if user:
-      self.email.errors.append("That email is already taken")
-      return False
-    else:
-      return True
-
-class SigninForm(Form):
-  email = TextField("Email", validators = [DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators = [DataRequired("Please enter a password.")])
-  submit = SubmitField("Sign In")
    
-  def __init__(self, *args, **kwargs):
-    Form.__init__(self, *args, **kwargs)
- 
-  def validate(self):
-    if not Form.validate(self):
-      return False
-     
-    user = User.query.filter_by(email = self.email.data.lower()).first()
-    if user and user.check_password(self.password.data):
-      return True
-    else:
-      self.email.errors.append("Invalid e-mail or password")
-      return False
-
-
-
-
-
-
-class LoginForm(Form):
-    openid = TextField('openid', validators = [DataRequired()])
-    remember_me = BooleanField('remember_me', default = False)
-    
 class RecipeForm(Form):
     recipe_name = TextField('recipe_name', validators = [DataRequired()])
     directions = TextAreaField('directions', validators = [Length(min = 0, max = 3000)])
