48b7c383da31d8a3c360a59c625ccad00f2bd673
MattBitt <mattsbitt@gmail.com>
2014-09-04 17:43:28 -0500

    cleaning up

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,12 @@ from wtforms.validators import DataRequired, Length, URL, Optional, NumberRange,
 from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
 from app.models import Recipe
 from app import db
+from wtforms.fields.html5 import URLField
+from wtforms.validators import url as url_validator
+from flask.ext.uploads import UploadSet, IMAGES
 
+
+images = UploadSet('images', IMAGES)
 
 class RecipeForm(Form):
     recipe_name = TextField('Recipe Name' , validators = [DataRequired( message='Recipe Name is required')])
@@ -20,4 +25,6 @@ class RecipeForm(Form):
     user_id = IntegerField('user_id')
     timestamp = DateField('Date', format='%Y-%m-%d')
     image_file = FileField('Image File')
+    #test_url = URLField(validators=[url_validator()])
     
+
